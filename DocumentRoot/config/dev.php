3ed3470348fe1ed66f8cbede14bcc4e2796e15a1
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2020-10-12 15:10:08 +0200

    Mise en place d'un routeur

--- DocumentRoot/config/dev.php
+++ DocumentRoot/config/dev.php
@@ -1,6 +1,6 @@
 <?php
-const HOST = 'localhost';
-const DB_NAME = 'blog';
+const HOST = 'mariadb';
+const DB_NAME = 'testdb';
 const CHARSET = 'utf8';
 const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'testuser';
