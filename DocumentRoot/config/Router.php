850a7e5b2eb3a16f34ace0606c94e91935ff15b2
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2020-10-14 17:28:03 +0200

    Finalisation MVC avec page 404 et 500

--- DocumentRoot/config/Router.php
+++ DocumentRoot/config/Router.php
@@ -3,16 +3,19 @@
 namespace App\config;
 
 use App\src\controller\FrontController;
+use App\src\controller\ErrorController;
 use Exception;
 
 class Router
 {
 
     private $frontController;
+    private $errorController;
 
     public function __construct()
     {
         $this->frontController = new FrontController();
+        $this->errorController = new ErrorController();
     }
 
     public function run()
@@ -22,13 +25,13 @@ class Router
                 if ($_GET['route'] === 'post') {
                     $this->frontController->post($_GET['postId']);
                 } else {
-                    echo 'page inconnue';
+                    $this->errorController->errorNotFound();
                 }
             } else {
                 $this->frontController->home();
             }
         } catch (Exception $e) {
-            echo 'Erreur';
+            $this->errorController->errorServer();
         }
     }
 }
