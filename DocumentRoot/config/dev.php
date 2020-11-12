f372ef8a01b0552bb601007dc05d581cfd959491
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2020-11-12 15:36:48 +0100

    Delete config/dev.php

--- DocumentRoot/config/dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-const HOST = 'mariadb';
-const DB_NAME = 'testdb';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
-const DB_USER = 'testuser';
-const DB_PASS = 'testpassword';
