081717adbff72a720afacf3bae2c32c1efda3960
Stéphane Jaulin <745810+sjaulin@users.noreply.github.com>
2021-01-06 20:37:06 +0100

    Move CKEDITOR script

--- html/templates/edit_article.php
+++ html/templates/edit_article.php
@@ -1,12 +1,4 @@
 <h2>Modifier l'article</h2>
 <div>
-    <?php include('form_article.php');?>
+    <?php include('form_article.php'); ?>
 </div>
-<script>
-        CKEDITOR.replace('ckeditor', {
-            filebrowserBrowseUrl: '/public/libraries/php/ckfinder/ckfinder.html',
-            filebrowserImageBrowseUrl: '/public/libraries/php/ckfinder/ckfinder.html?type=Images',
-            filebrowserUploadUrl: '/public/libraries/php/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files',
-            filebrowserImageUploadUrl: '/public/libraries/php/ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Images'
-        });
-    </script>
